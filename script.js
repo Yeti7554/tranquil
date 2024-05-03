f32ebc8dbdb012c78825f79f645547478e041e22
Abdullah <114833110+Yeti7554@users.noreply.github.com>
2024-05-03 16:24:33 -0600

    Added Homepage toggles

---
+++ script.js
@@ -0,0 +1,26 @@
+function toggleServices1() {
+    var servicesList = document.getElementById("servicesList1");
+    if (servicesList.style.display === "none") {
+        servicesList.style.display = "block";
+    } else {
+        servicesList.style.display = "none";
+    }
+}
+
+function toggleServices2() {
+    var servicesList = document.getElementById("servicesList2");
+    if (servicesList.style.display === "none") {
+        servicesList.style.display = "block";
+    } else {
+        servicesList.style.display = "none";
+    }
+}
+
+function toggleServices3() {
+    var servicesList = document.getElementById("servicesList3");
+    if (servicesList.style.display === "none") {
+        servicesList.style.display = "block";
+    } else {
+        servicesList.style.display = "none";
+    }
+}
